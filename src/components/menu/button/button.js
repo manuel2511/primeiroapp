01099c998a7e5e65fff232c91786d59df2f494eb
Manuel Machado <tekmanuel@gmail.com>
2023-07-24 17:28:12 -0400

    Commit 20230724

---
+++ src/components/menu/button/button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './index.css';
+function Button() {
+    return (
+      <button>I'm a button</button>
+    );
+  }export default Button;
---
+++ src/components/menu/menu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import './index.css';
+import Button from './button/button';
+function Menu() {
+    return (
+        <div>
+            <Button/>
+        </div>
+    );
+  }
